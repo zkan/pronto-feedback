ab9c14bc2a0039b8d26eb02124c803419ebe646e
Kan Ouivirach <kan@prontomarketing.com>
2016-08-27 06:30:01 +0700

    Save fid, creation_date, last_modification_date, question_asked in feedback

--- pronto_feedback/feedback/models.py
+++ pronto_feedback/feedback/models.py
@@ -1,7 +1,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 
 class Feedback(models.Model):
+    fid = models.CharField(max_length=50)
+    creation_date = models.DateTimeField(
+        default=timezone.now,
+        blank=True
+    )
+    last_modification_date = models.DateTimeField(
+        default=timezone.now,
+        blank=True
+    )
+    question_asked = models.TextField()
     message = models.TextField()
