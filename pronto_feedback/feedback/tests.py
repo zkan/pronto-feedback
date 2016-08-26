2b91cbd0fcb2325dee09ebe70cd0bdf3c29a16d2
Kan Ouivirach <kan@prontomarketing.com>
2016-08-26 22:39:02 +0700

    Create feedback model with message field

--- pronto_feedback/feedback/tests.py
+++ pronto_feedback/feedback/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Feedback
+
+
+class FeedbackTest(TestCase):
+    def test_save_feedback_should_have_message(self):
+        feedback = Feedback()
+        feedback.message = "I'm happy"
+        feedback.save()
+
+        feedback = Feedback.objects.latest('id')
+
+        self.assertEqual(feedback.message, "I'm happy")
