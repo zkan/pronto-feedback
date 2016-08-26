7667fb83440a887473ebfbabbfd04f68635bb2fd
Kan Ouivirach <kan@prontomarketing.com>
2016-08-26 21:11:59 +0700

    Call OfficeVibe feedback API

--- pronto_feedback/officevibe/tests.py
+++ pronto_feedback/officevibe/tests.py
@@ -1,3 +1,15 @@
+from mock import patch
+
 from django.test import TestCase
 
-# Create your tests here.
+from .apis import OfficeVibeManager
+
+
+class OfficeVibeManagerTest(TestCase):
+    @patch('officevibe.apis.requests.get')
+    def test_get_feedback_should_call_officevibe_api(self, mock):
+        manager = OfficeVibeManager()
+        manager.get_feedback()
+        mock.assert_called_once_with(
+            'https://app.officevibe.com/api/v2/feedback'
+        )
