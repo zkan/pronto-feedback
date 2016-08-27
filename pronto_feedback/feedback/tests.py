f80a325a7f6f985f3e2422e6a365c3f21155a739
Kan Ouivirach <kan@prontomarketing.com>
2016-08-27 11:31:54 +0700

    Show feedback data on feedback page (feedback id, message, and tags)

--- pronto_feedback/feedback/tests.py
+++ pronto_feedback/feedback/tests.py
@@ -47,11 +47,29 @@ class FeedbackViewTest(TestCase):
     def test_feedback_page_should_have_feedback_table(self):
         response = self.client.get(self.url)
 
-        expected = '<th>Feedback ID</th>'
+        expected = '<th width="15%">Feedback ID</th>'
         self.assertContains(response, expected, status_code=200)
         expected = '<th>Message</th>'
         self.assertContains(response, expected, status_code=200)
-        expected = '<th>Tags</th>'
+        expected = '<th width="20%">Tags</th>'
+        self.assertContains(response, expected, status_code=200)
+
+    def test_feedback_page_should_show_feedback_data(self):
+        feedback = Feedback.objects.create(
+            fid='57504457a6cca38a00cfb4f88',
+            message="I'm happy",
+        )
+        suggestion_box_tag = Tag.objects.create(name='Suggestion Box')
+        good_tag = Tag.objects.create(name='good')
+        feedback.tags.add(suggestion_box_tag)
+        feedback.tags.add(good_tag)
+
+        response = self.client.get(self.url)
+
+        expected = '<td>57504457a6cca38a00cfb4f88</td>'
+        expected += '<td>I&#39;m happy</td>'
+        expected += '<td><span class="label label-info">Suggestion Box</span>'
+        expected += '&nbsp;<span class="label label-info">good</span>&nbsp;'
         self.assertContains(response, expected, status_code=200)
 
 
--- pronto_feedback/feedback/views.py
+++ pronto_feedback/feedback/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from .models import Feedback
+
 
 class FeedbackView(TemplateView):
     template_name = 'index.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        feedback = Feedback.objects.all()
+
+        return render(
+            request,
+            self.template_name,
+            {
+                'feedback': feedback
+            }
+        )
