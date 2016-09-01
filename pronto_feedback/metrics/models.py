3a82b85a3df14cb5ae43731314dad1c00a6add1b
Kan Ouivirach <kan@prontomarketing.com>
2016-09-01 08:44:26 +0700

    Create metric model

--- pronto_feedback/metrics/models.py
+++ pronto_feedback/metrics/models.py
@@ -2,4 +2,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Metric(models.Model):
+    name = models.CharField(blank=True, max_length=50)
